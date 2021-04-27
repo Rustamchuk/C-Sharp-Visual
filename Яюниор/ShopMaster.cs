2d6ab89c93e2cc5a835f9583081ec860bc11b0e5
Rustamchuk <hgnmkoujm@icoud.com>
2021-04-27 22:41:39 +0300

    Update ShopMaster.cs

--- Яюниор/ShopMaster.cs
+++ Яюниор/ShopMaster.cs
@@ -8,25 +8,135 @@ namespace Яюниор
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ЗДРАВСТВУЙ МОЙ ДРУГ!");
-            Console.WriteLine("Что ж ты зашел ко мне в магазин оружий, что же ты хочешь сегодня?");
+            Product cucumber = new Product("Огурец", 200);
+            Product tomato = new Product("Помидор", 150);
+            Product apple = new Product("Яблоко", 250);
+            Product meet = new Product("Мясо", 500);
+            Product bread = new Product("Хлеб", 400);
+            Product fish = new Product("Рыба", 450);
+            Product sweets = new Product("Конфеты", 350);
+            Product lemon = new Product("Лимон", 100);
+            List<Product> products = new List<Product> { cucumber, tomato, apple, meet, bread, fish, sweets, lemon };
 
-            while (true)
+            int money = 1500;
+
+            Seller seller = new Seller(products);
+            Visitor visitor = new Visitor(money);
+
+            bool buying = true;
+
+
+            Console.WriteLine("ЗДРАВСТВУЙ МОЙ ДРУГ!");
+            Console.WriteLine("Что ж ты зашел ко мне в магазин, что же ты хочешь сегодня?");
+
+            while (buying)
             {
-                Console.WriteLine("");
+                Console.WriteLine("1 - купить что-то\n2 - показать что у тебя есть\n3 - выйти");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Product product = seller.Sell();
+
+                        if (product.Cost <= visitor.Money)
+                        {
+                            seller.GiveProduct(product);
+                            visitor.AddProduct(product);
+
+                            if (product.Name == "Гнилой помидор")
+                                Console.WriteLine("Вы где-то ошиблись, вот вам гнилой помидор");
+                        }
+                        else
+                        {
+                            Console.WriteLine("У тебя не хватает денег!");
+                        }
+                        break;
+                    case "2":
+                        visitor.ShowBag();
+                        break;
+                    case "3":
+                        buying = false;
+                        break;
+                }
             }
+        }
+    }
+
+    class Seller
+    {
+        private List<Product> _products = new List<Product>();
+
+        public Seller(List<Product> products)
+        {
+            _products = products;
+        }
+
+        public Product Sell()
+        {
+            string visitorAnswer;
+
+            ShowProducts();
+
+            Console.WriteLine("Напиши назавание продукта и мы посмотрим можешь ли ты это купить");
+            visitorAnswer = Console.ReadLine();
+
+            foreach (var product in _products)
+            {
+                if (product.Name == visitorAnswer)
+                    return product;
+            }
+            return new Product("Гнилой помидор", 0);
+        }
+
+        public void GiveProduct(Product product)
+        {
+            _products.Remove(product);
+        }
+
+        private void ShowProducts()
+        {
+            Console.WriteLine();
+
+            foreach (var product in _products)
+            {
+                Console.WriteLine($"{product.Name}-{product.Cost}");
+            }
+
+            Console.WriteLine();
         }
     }
 
     class Visitor
     {
+        private List<Product> _products = new List<Product>();
+        public int Money { get; private set; }
 
+        public Visitor(int money)
+        {
+            Money = money;
+        }
+
+        public void AddProduct(Product product)
+        {
+            Money -= product.Cost;
+            _products.Add(product);
+        }
+
+        public void ShowBag()
+        {
+            Console.WriteLine($"\n\nДеньги - {Money}\n\n");
+            foreach (var product in _products)
+            {
+                Console.Write($"-{product.Name}-");
+            }
+            Console.WriteLine();
+        }
     }
 
     class Product
     {
-        public string Name; 
-        public int Cost;
+        public string Name { get; private set; } 
+        public int Cost { get; private set; }
 
         public Product(string name, int money)
         {
