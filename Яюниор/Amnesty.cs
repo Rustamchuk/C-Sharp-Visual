be895ea8b903c8f27eb08a2540bb05c1433e7479
Rustamchuk <hgnmkoujm@icoud.com>
2021-05-04 19:55:51 +0300

    Create Amnesty.cs

---
+++ Яюниор/Amnesty.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Яюниор
+{
+    class Amnesty
+    {
+        static void Main(string[] args)
+        {
+            Criminal andrey = new Criminal("Andrey", "political");
+            Criminal bob = new Criminal("Bob", "kill");
+            Criminal victor = new Criminal("Victor", "political");
+            Criminal leonardo = new Criminal("Leonardo", "thief");
+            Criminal misha = new Criminal("Misha", "kill");
+            Criminal nikita = new Criminal("Nikita", "political");
+
+            List<Criminal> criminals = new List<Criminal> { andrey, bob, victor, leonardo, misha, nikita};
+
+            ShowCriminals(criminals);
+
+            criminals = criminals.OrderBy(criminals => criminals.Reason).TakeWhile(criminals => criminals.Reason != "political").Union(criminals.OrderByDescending(criminals => criminals.Reason).TakeWhile(criminals => criminals.Reason != "political")).ToList<Criminal>();
+
+            ShowCriminals(criminals);
+        }
+
+        static void ShowCriminals(List<Criminal> criminals)
+        {
+            foreach (var criminal in criminals)
+            {
+                Console.WriteLine($"{criminal.Name} - {criminal.Reason}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Criminal
+    {
+        public string Name { get; private set; }
+        public string Reason { get; private set; }
+
+        public Criminal(string name, string reason)
+        {
+            Name = name;
+            Reason = reason;
+        }
+    }
+}
